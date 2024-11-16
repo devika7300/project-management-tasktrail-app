e71d9e80bb9273b72ca6ce760398a35297683484
Devika <devikavid@gmail.com>
2024-11-15 23:32:19 -0500

    redirect images to s3

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -8,16 +8,16 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "pm-s3-images.s3.us-east-2.amazonaws.com",
-  //       port: "",
-  //       pathname: "/**",
-  //     }
-  //   ]
-  // }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pm-tt-s3-images.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -25,7 +25,7 @@ const Sidebar = () => {
             {/* TOP LOGO */}
             <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
                 <div className="text-xl font-bold text-gray-800 dark:text-white">
-                EDLIST
+                DEVLIST
                 </div>
                 {isSidebarCollapsed ? null : (
                 <button
@@ -41,14 +41,14 @@ const Sidebar = () => {
             {/* TEAM */}
             <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
                 <Image
-                src="/logo.png"
+                src="https://pm-tt-s3-images.s3.us-east-1.amazonaws.com/logo.png"
                 alt="Logo"
                 width={40}
                 height={40}
                 />
                 <div>
                 <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
-                    EDROH TEAM
+                    DEVIKA TEAM
                 </h3>
                 <div className="mt-1 flex items-start gap-2">
                     <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
